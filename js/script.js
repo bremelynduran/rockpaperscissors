a357019f7f21bd84362f8d114b9289e156190df8
Bremelyn Duran <bremelynduran@ccpaedu.com>
2018-10-19 01:30:58 +0000

    Click Handler

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 // *************************** YOUR CODE BELOW *******************************
+$("button").click(function(){
+    var input = $("#input").val();
+    $("#userChoice").text(input);
+});
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
