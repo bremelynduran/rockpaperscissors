076b71f2cd498f52544eea49cf481284909a82c8
Bremelyn Duran <bremelynduran@ccpaedu.com>
2018-10-26 01:30:21 +0000

    day3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-// *************************** YOUR CODE BELOW *******************************
+
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randonNumber = 0;
+
 $("button").click(function(){
     var input = $("#input").val();
+    userChoice = input;
     $("#userChoice").text(input);
+    var randomNumber = Math.random();
+    if (randomNumber > .7)
+    {
+        computerChoice = "rock";
+    }
+    if (randomNumber < .5)
+    {
+        computerChoice = "paper";
+    }
+    if (randomNumber < .7 && randomNumber > .5)
+    {
+        computerChoice = "scissors";
+    }
+    $("#computerChoice").text(computerChoice);
 });
+
+
+
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
